b442e48b88bbd517640dcd0ef88722086c9a70ba
idkhowtonamemyself <GitHub@fehse-d.de>
2023-11-24 19:21:19 +0100

    fixed

--- src/main.ts
+++ src/main.ts
@@ -32,10 +32,23 @@ var msAnzeige: HTMLSpanElement = document.getElementById("ms_Anzeige");
 const context: CanvasRenderingContext2D | null | undefined = canvas?.getContext('2d');
 
 const maxRadius: number = 20;
-const minRadius: number = 5
+const minRadius: number = 5;
+const gridScale: number = maxRadius * 3;
 const ballCount: number = 1000;
 var splitSize: number = 50;
 let physicsWorker: Worker = new Worker('build/physicsUpdate.js');
+let physicsWorker1: Worker = new Worker('build/physicsUpdate.js');
+let physicsWorker2: Worker = new Worker('build/physicsUpdate.js');
+let physicsWorker3: Worker = new Worker('build/physicsUpdate.js');
+
+let physicsWorkerPromise: Promise<Ball[]> = new Promise<Ball[]>(resolve => {
+});
+let physicsWorker1Promise: Promise<Ball[]> = new Promise<Ball[]>(resolve => {
+});
+let physicsWorker2Promise: Promise<Ball[]> = new Promise<Ball[]>(resolve => {
+});
+let physicsWorker3Promise: Promise<Ball[]> = new Promise<Ball[]>(resolve => {
+});
 
 var ballArray: Ball[] = [];
 
@@ -101,9 +114,29 @@ while (ballArray.length < ballCount) {
 
 var totalUpdateTime: number = Date.now();
 
-function draw(): void {
+function draw() {
 
-    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    context?.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    // if (tempBallArray[0]?.length) {
+    // let test = [];
+    // test.push(...tempBallArray[0]);
+    // test.push(...tempBallArray[1]);
+    // test.push(...tempBallArray[2]);
+    // test.push(...tempBallArray[3]);
+
+    // console.log(tempBallArray, [...tempBallArray[0], ...tempBallArray[1], ...tempBallArray[2], ...tempBallArray[3]], ballArray.length, tempBallArray[0].length, 'tempBallArray')
+
+    // ballArray = [...tempBallArray[0], ...tempBallArray[1], ...tempBallArray[2], ...tempBallArray[3]];
+    // console.log(ballArray);
+    // }
+
+    for (let i = 0; i < 4; i++) {
+        context?.beginPath();
+        context?.moveTo((window.innerWidth / 4) * i, 0);
+        context?.lineTo((window.innerWidth / 4) * i, , window.innerHeight);
+        context?.stroke();
+    }
 
     for (const ball of ballArray) { //DRAW
 
@@ -134,11 +167,12 @@ function draw(): void {
         // context.strokeStyle = "black";
         // context.stroke();
         //
+
         // context.beginPath();
-        // context.rect(ball.gridPlace.x * maxRadius - (maxRadius / 2), ball.gridPlace.y * maxRadius - (maxRadius / 2), maxRadius, maxRadius);
+        // context.rect(ball.gridPlace.x * maxRadius - (maxRadius / 2), ball.gridPlace.y * maxRadius - (maxRadius / 2), gridScale, gridScale);
         // context.strokeStyle = 'blue';
         // context.stroke();
-        //
+
         // context.beginPath();
         // context.rect(ball.gridPlace.x * maxRadius - ((maxRadius * 3) / 2), ball.gridPlace.y * maxRadius - ((maxRadius * 3) / 2), maxRadius * 3, maxRadius * 3);
         // context.strokeStyle = 'red';
@@ -160,19 +194,95 @@ function draw(): void {
         // context.stroke();
     }
 
-    // for (let index = 0; index < 5; index++) {
-        physicsWorker.postMessage([maxRadius, mouse, window.innerWidth, window.innerHeight, ballArray]);
-    // }
+//#region Grid Aufbau
 
-    setTimeout((): void => { //FIXME WARUM?
-        msAnzeige.innerText = (Date.now() - totalUpdateTime).toString();
+    let gridSize: vec2 = {
+        x: Math.ceil(window.innerWidth / gridScale),
+        y: Math.ceil(window.innerHeight / gridScale)
+    }; //Neuer Key
 
-        totalUpdateTime = Date.now();
+    let grid: Array<Array<Array<Ball>>> = [];
 
-        draw();
-    }, 1);
-}
+    for (let x = 0; x < gridSize.x; x++) {
+        let neuesArrayX: Array<Ball[]> = [];
 
-physicsWorker.onmessage = (e: MessageEvent): void => {
-    ballArray = e.data;
+        for (let y = 0; y < gridSize.y; y++) {
+            let neuesArrayY: Ball[] = [];
+            neuesArrayX.push(neuesArrayY);
+        }
+
+        grid.push(neuesArrayX);
+    }
+
+//#endregion
+
+//#region Zuordnen im Grid
+
+    for (const ball of ballArray) {
+        let gridKey: vec2 = {
+            x: Math.ceil(ball.position.x / gridScale),
+            y: Math.ceil(ball.position.y / gridScale)
+        }; //Neuer Key
+        ball.gridPlace = gridKey;
+
+        if (gridKey.x > grid.length - 1) {
+            gridKey.x = grid.length - 1;
+        }
+        if (gridKey.y > grid[0].length - 1) {
+            gridKey.y = grid[0].length - 1;
+        }
+
+        if (gridKey.x < 0) {
+            gridKey.x = 0;
+        }
+        if (gridKey.y < 0) {
+            gridKey.y = 0;
+        }
+
+        grid[gridKey.x][gridKey.y].push(ball);
+    }
+
+//#endregion
+
+// for (let index = 0; index < 5; index++) {
+
+    physicsWorker.postMessage([grid.slice(0, grid.length / 4), mouse, window.innerWidth, window.innerHeight]);
+    physicsWorker1.postMessage([grid.slice(grid.length / 4, (grid.length / 4) * 2), mouse, window.innerWidth, window.innerHeight]);
+    physicsWorker2.postMessage([grid.slice((grid.length / 4) * 2, (grid.length / 4) * 3), mouse, window.innerWidth, window.innerHeight]);
+    physicsWorker3.postMessage([grid.slice((grid.length / 4) * 3, grid.length), mouse, window.innerWidth, window.innerHeight]);
+
+    physicsWorkerPromise = new Promise<Ball[]>(resolve => {
+        physicsWorker.onmessage = (e: MessageEvent): void => {
+            // console.log("Worker0", e.data);
+            resolve(e.data);
+        }
+    });
+    physicsWorker1Promise = new Promise<Ball[]>(resolve => {
+        physicsWorker1.onmessage = (e: MessageEvent): void => {
+            // console.log("Worker1", e.data);
+            resolve(e.data);
+        }
+    });
+    physicsWorker2Promise = new Promise<Ball[]>(resolve => {
+        physicsWorker2.onmessage = (e: MessageEvent): void => {
+            // console.log("Worker2", e.data);
+            resolve(e.data);
+        }
+    });
+    physicsWorker3Promise = new Promise<Ball[]>(resolve => {
+        physicsWorker3.onmessage = (e: MessageEvent): void => {
+            // console.log("Worker3", e.data);
+            resolve(e.data);
+        }
+    });
+    Promise.all([physicsWorkerPromise, physicsWorker1Promise, physicsWorker2Promise, physicsWorker3Promise])
+        .then(values => {
+            // console.log("TEST")
+            ballArray = [...values[0], ...values[1], ...values[2], ...values[3]];
+            msAnzeige.innerText = (Date.now() - totalUpdateTime).toString();
+
+            totalUpdateTime = Date.now();
+            // console.clear();
+            draw();
+        });
 }
--- src/physicsUpdate.ts
+++ src/physicsUpdate.ts
@@ -14,10 +14,11 @@ function ForceField(position1: vec2, position2: vec2, radius: number): vec2 {
         y: position1.y - position2.y
     }
 
-    vectorLaenge = map_range(vectorLaenge, 0, radius, 1, 0.001);
+    let vectorLaengeT = map_range(vectorLaenge, 0, radius, 1, 0.001);
+    // console.log(vectorLaenge, vectorLaengeT, 0, radius, 1, 0.001);
 
-    richtungsVector.x *= vectorLaenge;
-    richtungsVector.y *= vectorLaenge;
+    richtungsVector.x *= vectorLaengeT;
+    richtungsVector.y *= vectorLaengeT;
 
     return richtungsVector
 }
@@ -43,172 +44,197 @@ function colorUpdate(color: number, stepSize: number): number {
 
 //#endregion
 
-onmessage = function (e) {
+onmessage = async function (e) {
 
-    let maxRadius: number = e.data[0];
+    let grid: Array<Array<Ball[]>> = e.data[0];
     let mouse: Mouse = e.data[1];
     let windowInnerWidth: number = e.data[2];
     let windowInnerHeight: number = e.data[3];
-    let ballArray: Ball[] = e.data[4];
+    let ballArray: Ball[] = [];
 
     let frameTime: number = Date.now() - deltaTime;
 
     deltaTime = Date.now();
 
-    //#region Grid Aufbau
+    grid.forEach(yArray => {
+        yArray.forEach(innerBallArray => {
 
-    let gridSize = {x: Math.ceil(windowInnerWidth / maxRadius), y: Math.ceil(windowInnerHeight / maxRadius)}; //Neuer Key
+            // #region Phys Update / Collisions mit Border
+            innerBallArray.forEach(ball => {
 
-    let grid: Array<Array<Ball[]>> = [];
+                // ball.rotationVector.x = ball.rotationVector.x > 2 ? getRandomArbitrary(-2, 0) : ball.rotationVector.x + .001;
+                // ball.rotationVector.y = ball.rotationVector.y > 2 ? getRandomArbitrary(-2, 0) : ball.rotationVector.y + .001;
 
-    for (let x = 0; x < gridSize.x; x++) {
-        let neuesArrayX: Array<Ball[]> = [];
+                ball.position.x += multiplayArrays(ball.velocity, ball.rotationVector).x * ((frameTime > 50 ? 50 : frameTime) / 10); //TODO simplify
+                ball.position.y += multiplayArrays(ball.velocity, ball.rotationVector).y * ((frameTime > 50 ? 50 : frameTime) / 10);
 
-        for (let y = 0; y < gridSize.y; y++) {
-            let neuesArrayY: Ball[] = [];
-            neuesArrayX.push(neuesArrayY);
-        }
+                if (ball.position.x + ball.radius > windowInnerWidth) { //Border Rechts
+                    ball.velocity.x *= -1;
+                    ball.position.x = windowInnerWidth - ball.radius;
 
-        grid.push(neuesArrayX);
-    }
-
-    //#endregion
-
-    //#region Zuordnen im Grid
-
-    for (const ball of ballArray) {
-        let gridKey = {x: Math.ceil(ball.position.x / maxRadius), y: Math.ceil(ball.position.y / maxRadius)}; //Neuer Key
-        ball.gridPlace = gridKey;
-
-        if (gridKey.x > grid.length - 1) {
-            gridKey.x = grid.length - 1;
-        }
-        if (gridKey.y > grid[0].length - 1) {
-            gridKey.y = grid[0].length - 1;
-        }
-
-        if (gridKey.x < 0) {
-            gridKey.x = 0;
-        }
-        if (gridKey.y < 0) {
-            gridKey.y = 0;
-        }
-
-        grid[gridKey.x][gridKey.y].push(ball);
-    }
-
-    //#endregion
-
-    for (const ball of ballArray) {
-
-        //#region Collision Update
-
-        let ballsToScan: Ball[] = [];
-        let start = {x: ball.gridPlace.x - 1, y: ball.gridPlace.y - 1}
-
-        for (let x = start.x; x < start.x + 3; x++) { //sammelt alle kacheln die getestet werden müssen.
-
-            if (x < 0 || x > grid.length - 1) {
-                break
-            }
-
-            for (let y = start.y; y < start.y + 3; y++) {
-
-                if (y < 0 || y > grid[0].length - 1) {
-                    break
-                }
-
-                let foundGridTiel = grid[x][y];
-
-                if (foundGridTiel.length) {
-
-                    foundGridTiel.forEach(ballInGrid => {
-                        if (ballInGrid.index != ball.index && !ballsToScan.includes(ballInGrid)) {
-                            ballsToScan.push(ballInGrid);
-                        }
-                    })
-                }
-            }
-        }
-
-        //#region MousColl
-
-        let dist = Dist(mouse.position, ball.position);
-
-        if (dist < ball.radius + mouse.radius) {
-
-            let collForce = ForceField(ball.position, mouse.position, mouse.radius + ball.radius);
-
-            var collLaenge = Math.sqrt(Math.pow(collForce.x, 2) + Math.pow(collForce.y, 2));
-            ball.position.x += (collForce.x / collLaenge) * Math.abs(ball.radius + mouse.radius - dist);
-            ball.position.y += (collForce.y / collLaenge) * Math.abs(ball.radius + mouse.radius - dist);
-
-            ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
-
-        }
-
-        //#endregion
-
-        // #region Collisions mit balls
-
-        ball.near = ballsToScan;
-
-        if (ballsToScan.length) {
-
-            // console.log("Near");
-
-            ballsToScan.forEach(collBall => {
-
-                let dist = Dist(collBall.position, ball.position);
-
-                if (dist < ball.radius + collBall.radius) {
-
-                    let collisionForce: vec2 = ForceField(ball.position, collBall.position, collBall.radius + ball.radius);
-
-                    ball.position.x += collisionForce.x * frameTime / 10;
-                    ball.position.y += collisionForce.y * frameTime / 10;
+                    ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
+                } else if (ball.position.x - ball.radius < 0) { //Border Links
+                    ball.velocity.x *= -1;
+                    ball.position.x = ball.radius;
 
                     ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
                 }
-            })
-        }
 
-        //#endregion
+                if (ball.position.y + ball.radius > windowInnerHeight) { //Border Oben
+                    ball.velocity.y *= -1;
+                    ball.position.y = windowInnerHeight - ball.radius;
 
-        //#region Phys Update / Collisions mit Border
+                    ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
+                } else if (ball.position.y - ball.radius < 0) { //Border Unten
+                    ball.velocity.y *= -1;
+                    ball.position.y = ball.radius;
 
-        ball.rotationVector.x = ball.rotationVector.x > 2 ? getRandomArbitrary(-2, 0) : ball.rotationVector.x + .001;
-        ball.rotationVector.y = ball.rotationVector.y > 2 ? getRandomArbitrary(-2, 0) : ball.rotationVector.y + .001;
+                    ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
+                }
+            });
+            //#regionend
 
-        ball.position.x += multiplayArrays(ball.velocity, ball.rotationVector).x * ((frameTime > 50 ? 50 : frameTime) / 10); //TODO simplify
-        ball.position.y += multiplayArrays(ball.velocity, ball.rotationVector).y * ((frameTime > 50 ? 50 : frameTime) / 10);
+            //#region Collision
+            if (innerBallArray.length > 1) {
 
-        if (ball.position.x + ball.radius > windowInnerWidth) { //Border Rechts
-            ball.velocity.x *= -1;
-            ball.position.x = windowInnerWidth - ball.radius;
+                let ballScanned: Ball[] = [];
 
-            ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
-        } else if (ball.position.x - ball.radius < 0) { //Border Links
-            ball.velocity.x *= -1;
-            ball.position.x = ball.radius;
+                innerBallArray.forEach(ball => {
+                    innerBallArray.forEach(otherBalls => {
+                        if (ball == otherBalls || ballScanned.includes(otherBalls)) return;
 
-            ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
-        }
+                        ballScanned.push(ball);
 
-        if (ball.position.y + ball.radius > windowInnerHeight) { //Border Oben
-            ball.velocity.y *= -1;
-            ball.position.y = windowInnerHeight - ball.radius;
+                        let dist = Dist(ball.position, otherBalls.position);
+                        if (dist < ball.radius + otherBalls.radius) {
 
-            ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
-        } else if (ball.position.y - ball.radius < 0) { //Border Unten
-            ball.velocity.y *= -1;
-            ball.position.y = ball.radius;
+                            let collisionForce: vec2 = ForceField(ball.position, otherBalls.position, otherBalls.radius + ball.radius);
 
-            ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
-        }
-    }
+                            ball.position.x += collisionForce.x * frameTime / 10;
+                            ball.position.y += collisionForce.y * frameTime / 10;
+
+                            ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
+                        }
+                    });
+                });
+            }
+            //#regionend
+
+            innerBallArray.forEach(ball => {
+                ballArray.push(ball); //add when done
+            });
+        });
+    });
+    postMessage(ballArray);
+
+    // for (const ball of ballArray) {
+    //
+    //     //#region Collision Update
+    //
+    //     let ballsToScan: Ball[] = [];
+    //     let start = {x: ball.gridPlace.x - 1, y: ball.gridPlace.y - 1}
+    //
+    //     for (let x = start.x; x < start.x + 3; x++) { //sammelt alle kacheln die getestet werden müssen.
+    //
+    //         if (x < 0 || x > grid.length - 1) {
+    //             break
+    //         }
+    //
+    //         for (let y = start.y; y < start.y + 3; y++) {
+    //
+    //             if (y < 0 || y > grid[0].length - 1) {
+    //                 break;
+    //             }
+    //
+    //             let foundGridTiel = grid[x][y];
+    //
+    //             if (foundGridTiel.length) {
+    //
+    //                 foundGridTiel.forEach(ballInGrid => {
+    //                     if (ballInGrid.index != ball.index && !ballsToScan.includes(ballInGrid)) {
+    //                         ballsToScan.push(ballInGrid);
+    //                     }
+    //                 })
+    //             }
+    //         }
+    //     }
+    //
+    //     //#region MousColl
+    //
+    //     let dist = Dist(mouse.position, ball.position);
+    //
+    //     if (dist < ball.radius + mouse.radius) {
+    //
+    //         let collForce = ForceField(ball.position, mouse.position, mouse.radius + ball.radius);
+    //
+    //         var collLaenge = Math.sqrt(Math.pow(collForce.x, 2) + Math.pow(collForce.y, 2));
+    //         ball.position.x += (collForce.x / collLaenge) * Math.abs(ball.radius + mouse.radius - dist);
+    //         ball.position.y += (collForce.y / collLaenge) * Math.abs(ball.radius + mouse.radius - dist);
+    //
+    //         ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
+    //
+    //     }
+    //
+    //     //#endregion
+    //
+    //     // #region Collisions mit balls
+    //
+    //     ball.near = ballsToScan;
+    //
+    //     if (ballsToScan.length) {
+    //
+    //         // console.log("Near");
+    //
+    //         ballsToScan.forEach(collBall => {
+    //
+    //             let dist = Dist(collBall.position, ball.position);
+    //
+    //             if (dist < ball.radius + collBall.radius) {
+    //
+    //                 let collisionForce: vec2 = ForceField(ball.position, collBall.position, collBall.radius + ball.radius);
+    //
+    //                 ball.position.x += collisionForce.x * frameTime / 10;
+    //                 ball.position.y += collisionForce.y * frameTime / 10;
+    //
+    //                 ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
+    //             }
+    //         })
+    //     }
+    //
+    //     //#endregion
+    //
+    //     //#region Phys Update / Collisions mit Border
+    //
+    //     ball.rotationVector.x = ball.rotationVector.x > 2 ? getRandomArbitrary(-2, 0) : ball.rotationVector.x + .001;
+    //     ball.rotationVector.y = ball.rotationVector.y > 2 ? getRandomArbitrary(-2, 0) : ball.rotationVector.y + .001;
+    //
+    //     ball.position.x += multiplayArrays(ball.velocity, ball.rotationVector).x * ((frameTime > 50 ? 50 : frameTime) / 10); //TODO simplify
+    //     ball.position.y += multiplayArrays(ball.velocity, ball.rotationVector).y * ((frameTime > 50 ? 50 : frameTime) / 10);
+    //
+    //     if (ball.position.x + ball.radius > windowInnerWidth) { //Border Rechts
+    //         ball.velocity.x *= -1;
+    //         ball.position.x = windowInnerWidth - ball.radius;
+    //
+    //         ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
+    //     } else if (ball.position.x - ball.radius < 0) { //Border Links
+    //         ball.velocity.x *= -1;
+    //         ball.position.x = ball.radius;
+    //
+    //         ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
+    //     }
+    //
+    //     if (ball.position.y + ball.radius > windowInnerHeight) { //Border Oben
+    //         ball.velocity.y *= -1;
+    //         ball.position.y = windowInnerHeight - ball.radius;
+    //
+    //         ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
+    //     } else if (ball.position.y - ball.radius < 0) { //Border Unten
+    //         ball.velocity.y *= -1;
+    //         ball.position.y = ball.radius;
+    //
+    //         ball.TEST_color = colorUpdate(ball.TEST_color, frameTime / 10);
+    //     }
+    // }
 
     //#endregion
-
-    postMessage(ballArray);
 }
